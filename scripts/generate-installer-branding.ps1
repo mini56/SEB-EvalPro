0b98c277bcc0dc6cb59263bfb90de352db6b07ee
mini56 <48644212+mini56@users.noreply.github.com>
2026-09-11 14:32:06 +0200

    Join installer SEB-eval-PRO wordmark

--- scripts/generate-installer-branding.ps1
+++ scripts/generate-installer-branding.ps1
@@ -106,10 +106,10 @@ $branding = @"
   </svg>
 
   <text x="205" y="268" font-family="Calibri, Arial, sans-serif" font-size="68" font-weight="700" fill="#0873bd">SEB-év</text>
-  <svg x="415" y="220" width="48" height="48" viewBox="19 0 20 20">
+  <svg x="397" y="220" width="48" height="48" viewBox="19 0 20 20">
     <path fill="#F9B233" d="$aPath"/>
   </svg>
-  <text x="458" y="268" font-family="Calibri, Arial, sans-serif" font-size="68" font-weight="700" fill="#0873bd">l-PRO</text>
+  <text x="436" y="268" font-family="Calibri, Arial, sans-serif" font-size="68" font-weight="700" fill="#0873bd">l-PRO</text>
 
   <text x="450" y="318" text-anchor="middle" font-family="Calibri Light, Calibri, Arial, sans-serif" font-size="27" font-weight="300" fill="#0873bd">Version $version</text>
 
