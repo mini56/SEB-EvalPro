b3566347d5c075487f1bfa5e4aec558e202015e7
mini56 <48644212+mini56@users.noreply.github.com>
2026-09-16 07:10:39 +0200

    165+ synthèse V7: corriger le remplacement syntaxique du runner

--- scripts/build165plus-natural-synthesis-v7-runner.js
+++ scripts/build165plus-natural-synthesis-v7-runner.js
@@ -5,6 +5,6 @@ let s=fs.readFileSync(source,'utf8');
 const bad="label.replace(/[.*+?^${}()|[\\]\\\\]/g,'\\\\$&')";
 const good="label.replace(/[.*+?^$()|[\\]\\\\{}]/g,'\\\\$&')";
 if(!s.includes(bad))throw new Error('Correctif syntaxique V7 introuvable');
-s=s.replace(bad,good);
+s=s.replace(bad,()=>good);
 fs.writeFileSync(temp,s,'utf8');
 try{require(temp)}finally{try{fs.unlinkSync(temp)}catch(_){}}
