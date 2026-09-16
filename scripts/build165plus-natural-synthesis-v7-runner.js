d6e753f070b9ff0d38759911228c1ff47594582d
mini56 <48644212+mini56@users.noreply.github.com>
2026-09-16 07:09:11 +0200

    165+ synthèse V7: corriger le runner du dernier test sans IA

---
+++ scripts/build165plus-natural-synthesis-v7-runner.js
@@ -0,0 +1,10 @@
+const fs=require('fs'),path=require('path');
+const source=path.join(__dirname,'build165plus-natural-synthesis-v7.js');
+const temp=path.join(__dirname,'.build165plus-natural-synthesis-v7-runtime.js');
+let s=fs.readFileSync(source,'utf8');
+const bad="label.replace(/[.*+?^${}()|[\\]\\\\]/g,'\\\\$&')";
+const good="label.replace(/[.*+?^$()|[\\]\\\\{}]/g,'\\\\$&')";
+if(!s.includes(bad))throw new Error('Correctif syntaxique V7 introuvable');
+s=s.replace(bad,good);
+fs.writeFileSync(temp,s,'utf8');
+try{require(temp)}finally{try{fs.unlinkSync(temp)}catch(_){}}
