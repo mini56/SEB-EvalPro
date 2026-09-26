bee973bf3ea659c8afa66222e855d8868b4293ef
mini56 <48644212+mini56@users.noreply.github.com>
2026-09-26 19:46:14 +0200

    Externalize remaining source HTML scripts

---
+++ source/js/introbrique-page.js
@@ -0,0 +1,11 @@
+
+const video = document.getElementById("introVideo");
+const fade = document.getElementById("fade");
+
+video.onended = () => {
+  fade.style.opacity = 1;
+
+  setTimeout(() => {
+    window.location.href = "brique.html";
+  }, 1500);
+};
